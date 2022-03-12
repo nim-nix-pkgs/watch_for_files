9702fc696e9717528b240c762cf435009f8c42b5
hugosenari <hugosenari@gmail.com>
2022-03-12 02:00:44 -0300

    chore: re index watch_for_files

---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''cross-platform file watcher with database'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-watch_for_files-main.flake = false;
+  inputs.src-watch_for_files-main.owner = "hamidb80";
+  inputs.src-watch_for_files-main.ref   = "refs/heads/main";
+  inputs.src-watch_for_files-main.repo  = "watch_for_files";
+  inputs.src-watch_for_files-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-watch_for_files-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-watch_for_files-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
